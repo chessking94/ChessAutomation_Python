c30cffd4df436bb714a701d8e1a3f413c75306f9
Ethan Hunt <chessking94@gmail.com>
2026-03-28 12:32:09 -0500

    Refactor UpdateMoveScores to use new base class

--- src/UpdateMoveScores.py
+++ src/UpdateMoveScores.py
@@ -1,14 +1,18 @@
 import logging
-import os
 
 import pyodbc
 
+from base import base
 
-def main():
-    """ This process identifies and updates the move scores for any unanalyzed game imports """
-    conn_str = os.getenv('ConnectionStringOdbcRelease')
-    with pyodbc.connect(conn_str) as conn:
-        sql_cmd = '''
+
+class UpdateMoveScores(base):
+    abbreviation = 'MOVESCORES'
+
+    def _go(self):
+        """ This process identifies and updates the move scores for any unanalyzed game imports """
+        conn_str = self.config.get('connectionString')
+        with pyodbc.connect(conn_str) as conn:
+            sql_cmd = '''
 SELECT fh.FileID
 
 FROM ChessWarehouse.lake.Games AS g
@@ -24,12 +28,15 @@ GROUP BY fh.FileID
 HAVING COUNT(g.GameID) = SUM(CASE WHEN g.AnalysisStatusID = 3 THEN 1 ELSE 0 END)  --all games were analyzed successfully
 AND COUNT(g.GameID) = SUM(CASE WHEN m.GameID IS NOT NULL THEN 1 ELSE 0 END)  --all games have yet to be scored
 '''
-    with conn.cursor() as csr:
-        csr.execute(sql_cmd)
-        fileids = [row[0] for row in csr.fetchall()]
+        with conn.cursor() as csr:
+            csr.execute(sql_cmd)
+            fileids = [row[0] for row in csr.fetchall()]
 
-        for f in fileids:
-            logging.info(f'Started updating move scores for FileID = {f}')
-            csr.execute(f'EXEC ChessWarehouse.dbo.UpdateMoveScores @fileid = {f}')
-            csr.commit()
-            logging.info(f'Ended updating move scores for FileID = {f}')
+            for f in fileids:
+                if self.test_mode:
+                    logging.info(f'FileID = {f} pending move scoring')
+                else:
+                    logging.info(f'Started updating move scores for FileID = {f}')
+                    csr.execute(f'EXEC ChessWarehouse.dbo.UpdateMoveScores @fileid = {f}')
+                    csr.commit()
+                    logging.info(f'Ended updating move scores for FileID = {f}')
